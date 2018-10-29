7ae024b3b28e454e429b6623c26b10e8f12d5f26
batlen <nick.batley@dvn.com>
2018-10-29 15:44:10 -0500

    Add probabilities to output and individual thresholds

--- hackathon/src/main/java/com/dvn/safety/SafetyOutput.java
+++ hackathon/src/main/java/com/dvn/safety/SafetyOutput.java
@@ -5,18 +5,28 @@ public class SafetyOutput {
     public boolean glasses;
     public boolean hardhat;
     public boolean coat;
+    public double glasses_probability;
+    public double hardhat_probability;
+    public double coat_probability;
 
-    public static float threshold = 0.5f;
+    public static float hardhat_threshold = 0.6f;
+    public static float glasses_threshold = 0.7f;
+    public static float coat_threshold = 0.5f;
 
     public SafetyOutput(MLOutput mlOutput) {
         for (MLOutput.Prediction prediction : mlOutput.predictions) {
             switch(prediction.tagName) {
                 case "Hard hat":
-                    hardhat = prediction.probability > threshold;
+                    hardhat = prediction.probability > hardhat_threshold;
+                    hardhat_probability = prediction.probability;
+                    break;
                 case "frc":
-                    coat = prediction.probability > threshold;
+                    coat = prediction.probability > coat_threshold;
+                    coat_probability = prediction.probability;
+                    break;
                 case "safety glass":
-                    glasses = prediction.probability > threshold;
+                    glasses = prediction.probability > glasses_threshold;
+                    glasses_probability = prediction.probability;
             }
         }
     }
